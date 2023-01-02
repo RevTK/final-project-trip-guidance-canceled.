6d0dd992d991f53434cc29031f2b4cfac368bb88
Revtk <trainst37@gmail.com>
2023-01-03 02:35:14 +0900

    기본디자인

--- Miracle/src/main/java/com/tm/mp/HomeController.java
+++ Miracle/src/main/java/com/tm/mp/HomeController.java
@@ -8,125 +8,162 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.tm.mp.account.AccountDAO;
+
 @Controller
 public class HomeController {
-	
+
 	@Autowired
 	private AccountDAO aDAO;
-	
+
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(HttpServletRequest req) {
 		aDAO.loginCheck(req);
 		req.setAttribute("contentPage", "home.jsp");
 		return "index";
 	}
-	
+
 	@RequestMapping(value = "/index.go", method = RequestMethod.GET)
 	public String indexGo(HttpServletRequest req) {
 		return home(req);
 	}
-	
+
 	@RequestMapping(value = "/account.login.go", method = RequestMethod.GET)
 	public String accountLoginGo(HttpServletRequest req) {
 		aDAO.loginCheck(req);
 		req.setAttribute("contentPage", "account/loginPage.jsp");
 		return "index";
 	}
-	
+
 	@RequestMapping(value = "/rec.main.go", method = RequestMethod.GET)
 	public String recMainGo(HttpServletRequest req) {
 		aDAO.loginCheck(req);
 		req.setAttribute("contentPage", "rec/recMain.jsp");
 		return "index";
 	}
-	
-	
+
 	@RequestMapping(value = "/myPage.main.go", method = RequestMethod.GET)
 	public String myPageMainGo(HttpServletRequest req) {
 		aDAO.loginCheck(req);
 		req.setAttribute("contentPage", "myPage/myPageMain.jsp");
 		return "index";
 	}
-	
+
 	@RequestMapping(value = "/event.main.go", method = RequestMethod.GET)
 	public String eventMainGo(HttpServletRequest req) {
 		aDAO.loginCheck(req);
 		req.setAttribute("contentPage", "event/eventMain.jsp");
 		return "index";
 	}
-	
-	//--디자인2---------------------------------------------------------
-	
+
+	// --디자인2---------------------------------------------------------
+
 	@RequestMapping(value = "/index2.go", method = RequestMethod.GET)
 	public String index2Go(HttpServletRequest req) {
 		aDAO.loginCheck(req);
 		req.setAttribute("contentPage", "home2.jsp");
 		return "index2";
 	}
-	
+
 	@RequestMapping(value = "/account2.login.go", method = RequestMethod.GET)
 	public String accountLoginGo2(HttpServletRequest req) {
 		aDAO.loginCheck(req);
 		req.setAttribute("contentPage", "account/loginPage.jsp");
 		return "index2";
 	}
-	
+
 	@RequestMapping(value = "/rec2.main.go", method = RequestMethod.GET)
 	public String recMainGo2(HttpServletRequest req) {
 		aDAO.loginCheck(req);
 		req.setAttribute("contentPage", "rec/recMain.jsp");
 		return "index2";
 	}
-	
+
 	@RequestMapping(value = "/myPage2.main.go", method = RequestMethod.GET)
 	public String myPageMainGo2(HttpServletRequest req) {
 		aDAO.loginCheck(req);
 		req.setAttribute("contentPage", "myPage/myPageMain.jsp");
 		return "index2";
 	}
-	
+
 	@RequestMapping(value = "/event2.main.go", method = RequestMethod.GET)
 	public String eventMainGo2(HttpServletRequest req) {
 		aDAO.loginCheck(req);
 		req.setAttribute("contentPage", "event/eventMain.jsp");
 		return "index2";
 	}
-	
-	//--디자인3---------------------------------------------------------
-	
+
+	// --디자인3---------------------------------------------------------
+
 	@RequestMapping(value = "/index3.go", method = RequestMethod.GET)
 	public String index3Go(HttpServletRequest req) {
 		aDAO.loginCheck(req);
 		req.setAttribute("contentPage", "home3.jsp");
 		return "index3";
 	}
-	
+
 	@RequestMapping(value = "/account3.login.go", method = RequestMethod.GET)
 	public String accountLoginGo3(HttpServletRequest req) {
 		aDAO.loginCheck(req);
 		req.setAttribute("contentPage", "account/loginPage.jsp");
 		return "index3";
 	}
-	
+
 	@RequestMapping(value = "/rec3.main.go", method = RequestMethod.GET)
 	public String recMainGo3(HttpServletRequest req) {
 		aDAO.loginCheck(req);
 		req.setAttribute("contentPage", "rec/recMain.jsp");
 		return "index3";
 	}
-	
+
 	@RequestMapping(value = "/myPage3.main.go", method = RequestMethod.GET)
 	public String myPageMainGo3(HttpServletRequest req) {
 		aDAO.loginCheck(req);
 		req.setAttribute("contentPage", "myPage/myPageMain.jsp");
 		return "index3";
 	}
-	
+
 	@RequestMapping(value = "/event3.main.go", method = RequestMethod.GET)
 	public String eventMainGo3(HttpServletRequest req) {
 		aDAO.loginCheck(req);
 		req.setAttribute("contentPage", "event/eventMain.jsp");
 		return "index3";
 	}
-	
+
+	// --디자인3---------------------------------------------------------
+
+	@RequestMapping(value = "/index4.go", method = RequestMethod.GET)
+	public String index4Go(HttpServletRequest req) {
+		aDAO.loginCheck(req);
+		req.setAttribute("contentPage", "home4.jsp");
+		return "index4";
+	}
+
+	@RequestMapping(value = "/account4.login.go", method = RequestMethod.GET)
+	public String accountLoginGo4(HttpServletRequest req) {
+		aDAO.loginCheck(req);
+		req.setAttribute("contentPage", "account/loginPage.jsp");
+		return "index4";
+	}
+
+	@RequestMapping(value = "/rec4.main.go", method = RequestMethod.GET)
+	public String recMainGo4(HttpServletRequest req) {
+		aDAO.loginCheck(req);
+		req.setAttribute("contentPage", "rec/recMain.jsp");
+		return "index4";
+	}
+
+	@RequestMapping(value = "/myPage4.main.go", method = RequestMethod.GET)
+	public String myPageMainGo4(HttpServletRequest req) {
+		aDAO.loginCheck(req);
+		req.setAttribute("contentPage", "myPage/myPageMain.jsp");
+		return "index4";
+	}
+
+	@RequestMapping(value = "/event4.main.go", method = RequestMethod.GET)
+	public String eventMainGo4(HttpServletRequest req) {
+		aDAO.loginCheck(req);
+		req.setAttribute("contentPage", "event/eventMain.jsp");
+		return "index4";
+	}
+
 }
--- Miracle/src/main/webapp/resources/js/index.js
+++ Miracle/src/main/webapp/resources/js/index.js
@@ -1,361 +1,114 @@
-$('.trigger').click(function () {
-  $('.page_portfolio').css('opacity', '1');
-  $('.page_portfolio').css('clip-path', 'polygon(0 100%, 100% 100%, 100% 0%, 0 0%)');
-  $('.logo img, .portfolio_home__title h1, .portfolio_home__title hr, .portfolio_home__title img.trigger').addClass('out');
-  $('.slider_inner').addClass('in');
-  setTimeout(function () {
-    $('.slider_inner').click();
-
-  }, 2500);
-});
-
-introComplete = false;
-
-setTimeout(function () {
-  introComplete = true;
-}, 2500);
-
-var scrollPosition = 0;
-window.addEventListener('wheel', function (e) {
-
-  if (e.deltaY > 0) {
-    scrollPosition += 10;
-    $('.content').css('top', -scrollPosition + 'px');
-    console.log(e.deltaY);
-    if (introComplete) {
-      $('.page_portfolio').css('opacity', '1');
-      $('.page_portfolio').css('clip-path', 'polygon(0 100%, 100% 100%, 100% 0%, 0 0%)');
-      $('.logo img, .portfolio_home__title h1, .portfolio_home__title hr, .portfolio_home__title img.trigger').addClass('out');
-      $('.slider_inner').addClass('in');
-      setTimeout(function () {
-        $('.slider_inner').click();
-
-      }, 2000);
-
-
-    }
-  } else {
-    scrollPosition -= 10;
-  }
-});
-
-$('.button').click(function () {
-  $(this).parent().addClass('clicked');
-  $(this).parent().parent().parent().addClass('clicked');
-  // $('.portfolio_home__work').css('opacity', 1);
-  $('.portfolio_home__work').addClass('expand');
-
-});
-
-// Gross but only a demo
-
-$(window).resize(function () {
-  $('.slider_inner').css('left', $(document).width() / 2 - $('.slider_inner__slide').width() / 2);
-});
-
-$('.slider_inner').css('left', $(document).width() / 2 - $('.slider_inner__slide').width() / 2);
-
-var dragging = false;
-var endPosition = 0;
-var threshold = 100;
-
-$('.slider_inner').click(function () {
-  $('.slider_inner__slide').css('animation', 'none');
-  $('.slider_inner__slide').css('transform', 'rotateY(0deg) scale(1)');
-});
-$('.slider_inner').mousedown(function (e) {
-  initX = e.clientX;
-  dragging = true;
-  // $('.slider_inner__slide').css('animation', 'none')
-  cursor.style.transition = `transform 0s 0s`;
-
-  console.log(cursor);
-});
-
-$('.slider_inner').mousemove(function (e) {
-
-  if (dragging) {
-    let mouseX = e.clientX;
-    difference = mouseX - initX;
-
-
-    // $('.slider_inner__slide').css('transform', 'rotateY(' + direction + pps /
-	// 110 + 'deg) scale(1)')
-    position = parseInt($('.slider_inner').css('transform').split(',')[4]);
-    // console.log('position is ' + position)
-    $('.slider_inner__slide:nth-of-type(' + parseInt(index + 1) + ') .image .overlay, .slider_inner__slide:nth-of-type(' + parseInt(index + 1) + ') .image .title, .slider_inner__slide:nth-of-type(' + parseInt(index + 1) + ') .image .cats, .slider_inner__slide:nth-of-type(' + parseInt(index + 1) + ') .image .button').css('opacity', 1 - Math.abs(difference / 200));
-
-    $('.slider_inner__slide:nth-of-type(' + parseInt(index + 1) + ') .image .overlay, .slider_inner__slide:nth-of-type(' + parseInt(index + 1) + ') .image .title, .slider_inner__slide:nth-of-type(' + parseInt(index + 1) + ') .image .cats, .slider_inner__slide:nth-of-type(' + parseInt(index + 1) + ') .image .button').css('transition', 'all .2s');
-
-    $('.slider_inner').css('transform', `translateX(${difference + endPosition}px) translateY(120px)`);
-
-
-  }
-});
-
-// 520 -40 -600 -1150
-
-// 560
-
-
-var offset = 760;
-var margin = 0;
-
-var index = 1;
-
-$('.slider_inner').mouseup(function () {
-  cursor.style.transition = `transform ${cursorSettings.transitionTime} ${cursorSettings.transitionEase}, width ${cursorSettings.expandSpeed}s .2s, height ${cursorSettings.expandSpeed}s .2s, opacity 1s .2s`;
-  endPosition = parseInt($('.slider_inner').css('transform').split(',')[4]);
-
-
-  if (difference < -160) {
-    console.log('snap to next');
-    if (index < 3) {
-      index++;
-      var threshold = offset - (offset + margin) * index;
-      $('.slider_inner').css('transform', `translateX(${threshold}px) translateY(120px)`);
-      endPosition = threshold;
-
-    } else {
-      var threshold = offset - (offset + margin) * index;
-      $('.slider_inner').css('transform', `translateX(${threshold}px) translateY(120px)`);
-      endPosition = threshold;
-
-    }
-
-  } else {
-    if (difference > 160) {
-      if (index > 0) {
-        index--;
-        var threshold = offset - (offset + margin) * index;
-        $('.slider_inner').css('transform', `translateX(${threshold}px) translateY(120px)`);
-        endPosition = threshold;
-
-      } else {
-        var threshold = offset - (offset + margin) * index;
-        $('.slider_inner').css('transform', `translateX(${threshold}px) translateY(120px)`);
-        endPosition = threshold;
-
-      }
-
-    } else {
-      var threshold = offset - (offset + margin) * index;
-      $('.slider_inner').css('transform', `translateX(${threshold}px) translateY(120px)`);
-      endPosition = threshold;
-
-    }
-  }
-
-  dragging = false;
-  console.log('.slider_inner__slide:nth-of-type(' + parseInt(index + 1) + ')');
-  $('.slider_inner__slide:nth-of-type(' + parseInt(index + 1) + ') .image .overlay, .slider_inner__slide:nth-of-type(' + parseInt(index + 1) + ') .image .title, .slider_inner__slide:nth-of-type(' + parseInt(index + 1) + ') .image .cats, .slider_inner__slide:nth-of-type(' + parseInt(index + 1) + ') .image .button').css('opacity', 1);
-  $('.slider_inner__slide').css('transform', 'rotateY(0deg) scale(1)');
-  $('.slideClone').hide();
-  $('.slideClone:nth-of-type(' + parseInt(index + 2) + ')').show();
-  difference = 0;
-});
-
-// https://gist.github.com/ripper234/5757309
-
-function drawMouseSpeedDemo() {
-  var mrefreshinterval = 30; // update display every 500ms
-  var lastmousex = -1;
-  var lastmousey = -1;
-  var lastmousetime;
-  var mousetravel = 0;
-  var mpoints = [];
-  var mpoints_max = 30;
-  var direction;
-
-  $('html').mousemove(function (e) {
-    var mousex = e.pageX;
-    var mousey = e.pageY;
-    if (lastmousex > -1) {
-      mousetravel += Math.max(Math.abs(mousex - lastmousex), Math.abs(mousey - lastmousey));
-    }
-    // console.log(mousex-lastmousex)
-
-    if (mousex - lastmousex > 0) {
-      direction = '+';
-    } else {
-      direction = '-';
-    }
-
-    // console.log(direction);
-
-    lastmousex = mousex;
-    lastmousey = mousey;
-  });
-  var mdraw = function () {
-    var md = new Date();
-    var timenow = md.getTime();
-    if (lastmousetime && lastmousetime != timenow) {
-      var pps = Math.round(mousetravel / (timenow - lastmousetime) * 1000);
-      mpoints.push(pps);
-      if (mpoints.length > mpoints_max)
-      mpoints.splice(0, 1);
-      mousetravel = 0;
-      // console.log(pps)
-      if (dragging) {
-        let velocity = .5 - pps / 40000;
-
-
-
-        $('.slider_inner__slide').css('transform', 'rotateY(' + direction + pps / 110 + 'deg) scale(1)');
-        $('.slider_inner__slide').css('transition', 'all ' + velocity + 's');
-        // console.log(velocity)
-      }
-
-    }
-    lastmousetime = timenow;
-    setTimeout(mdraw, mrefreshinterval);
-  };
-  // We could use setInterval instead, but I prefer to do it this way
-  setTimeout(mdraw, mrefreshinterval);
-};
-
-drawMouseSpeedDemo();
-
-/*
- * -------------------------------------------------
- * 
- * Dynamic cursor
- * 
- * ---------------------------------------------------
- */
-
-const cursorSettings = {
-  'class': 'dynamicCursor',
-  'size': '18',
-  'expandedSize': '40',
-  'expandSpeed': 0.4,
-  'background': 'rgba(161, 142, 218, 0.25)',
-  'opacity': '1',
-  'transitionTime': '1.4s',
-  'transitionEase': 'cubic-bezier(0.075, 0.820, 0.165, 1.000)',
-  'borderWidth': '0',
-  'borderColor': 'black',
-  'iconSize': '11px',
-  'iconColor': 'white',
-  'triggerElements': {
-    'trigger': {
-      'className': 'trigger',
-      'icon': '<i class="fa fa-plus"></i>' },
-
-    'trigger2': {
-      'className': 'slider_inner',
-      'icon': '<i class="fa fa-arrows-h"></i>' } } };
-
-
-
-
-
-function dynamicCursor(options) {
-
-  document.write('<link rel="stylesheet" href="https://maxcdn.icons8.com/fonts/line-awesome/1.1/css/line-awesome-font-awesome.min.css">');
-
-  var hold;
-  cursor = document.createElement('div');
-  let cursorIcon = document.createElement('div');
-
-  cursorIcon.classList.add('cursorIcon');
-  cursorIcon.style.position = 'absolute';
-  cursorIcon.style.fontFamily = 'Raleway';
-  cursorIcon.style.textTransform = 'uppercase';
-  cursorIcon.style.fontWeight = '800';
-  cursorIcon.style.textAlign = 'center';
-  cursorIcon.style.top = '50%';
-  cursorIcon.style.width = '100%';
-  cursorIcon.style.transform = 'translateY(-50%)';
-  cursorIcon.style.color = options.iconColor;
-  cursorIcon.style.fontSize = options.iconSize;
-  cursorIcon.style.opacity = 0;
-  cursorIcon.style.transition = `opacity ${options.expandSpeed}s`;
-
-  cursor.classList.add(options.class);
-  cursor.style.boxSizing = 'border-box';
-  cursor.style.width = `${options.size}px`;
-  cursor.style.height = `${options.size}px`;
-  cursor.style.borderRadius = `${options.expandedSize}px`;
-  cursor.style.opacity = 0;
-
-  cursor.style.pointerEvents = 'none';
-  cursor.style.zIndex = 999;
-  cursor.style.transition = `transform ${options.transitionTime} ${options.transitionEase}, width ${options.expandSpeed}s .2s, height ${options.expandSpeed}s .2s, opacity 1s .2s`;
-  cursor.style.border = `${options.borderWidth}px solid ${options.borderColor}`;
-  cursor.style.position = 'fixed';
-  cursor.style.background = options.background;
-
-  cursor.appendChild(cursorIcon);
-  document.body.appendChild(cursor);
-
-  setTimeout(function () {
-    cursor.style.opacity = options.opacity;
-  }, 500);
-
-  var idle;
-
-  document.onmousemove = e => {
-    console.log('test');
-    x = e.pageX;
-    y = e.pageY;
-
-    cursor.style.opacity = options.opacity;
-    clearInterval(idle);
-
-    idle = setTimeout(function () {
-      cursor.style.opacity = 0;
-    }, 4000);
-
-    cursor.style.top = '0';
-    cursor.style.left = '0';
-    cursor.style.transform = `translateX(calc(${x}px - 50%)) translateY(calc(${y}px - 50%))`;
-  };
-
-  for (i in options.triggerElements) {
-
-    let trigger = $(`.${options.triggerElements[i].className}`);
-
-    console.log(trigger);
-
-
-    let icon = options.triggerElements[i].icon;
-
-    if (!trigger) {
-      console.warn('You dont have any triggers');
-    } else {
-      trigger.each(function (el) {
-
-        console.log();
-        trigger[el].style.cursor = 'default';
-        trigger[el].addEventListener('mouseover', () => {
-          console.log('over');
-          cursor.style.width = `${options.expandedSize}px`;
-          cursor.style.height = `${options.expandedSize}px`;
-          cursorIcon.innerHTML = icon;
-          cursorIcon.style.opacity = 1;
-
-
-          console.log($(this));
-
-
-        });
-
-        trigger[el].addEventListener('mouseout', () => {
-          cursor.style.width = `${options.size}px`;
-          cursor.style.height = `${options.size}px`;
-          cursorIcon.style.opacity = 0;
-        });
-      });
-
-    }
-  }
+/*=============== SHOW MENU (모바일 네비바) ===============*/
+const navMenu = document.getElementById('nav-menu'), navToggle = document.getElementById('nav-toggle'), navClose = document.getElementById('nav-close')
+
+/*=============== MENU SHOW (모바일 네비바)===============*/
+/* Validate if constant exists */
+if (navToggle) {
+  navToggle.addEventListener('click', () => {
+    navMenu.classList.add('show-menu')
+  })
 }
 
-dynamicCursor(cursorSettings);
+/*=============== MENU SHOW (모바일 네비바)===============*/
+/* Validate if constant exists */
+if (navClose) {
+  navClose.addEventListener('click', () => {
+    navMenu.classList.remove('show-menu')
+  })
+}
 
-$('.back').click(function () {
-  $(this).parent().parent().removeClass('expand');
-});
+/*=============== REMOVE MENU MOBILE (메뉴클릭시 메뉴바가 사라지는 모션) ===============*/
+const navLink = document.querySelectorAll('.nav__link');
 
-// make go back from page to slider again
+const linkAction = () => {
+  const navMenu = document.getElementById('nav-menu')
+  // When we click on each nav__link, we remove the show-menu class
+  navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+const scrollHeader = () => {
+  const header = document.getElementById('header')
+  // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+  this.scrollY >= 50 ? header.classList.add('bg-header') : header.classList.remove('bg-header')
+  // bg-header css로도 연결
+}
+window.addEventListener('scroll', scrollHeader)
+
+/*=============== SHOW SCROLL UP 350내려가면 박스 스크롤 생김===============*/
+const scrollUp = () => {
+  const scrollUp = document.getElementById('scroll-up')
+  // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup
+  this.scrollY >= 350 ? scrollUp.classList.add('show-scroll') : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+/*=============== SCROLL SECTIONS ACTIVE LINK 보고있는 섹션 메뉴 색으로 표시===============*/
+const sections = document.querySelectorAll('section[id]')
+
+const scrollActive = () => {
+  const scrollY = window.pageYOffset
+
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute('id'),
+      sectionClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      sectionClass.classList.add('active-link')
+    } else {
+      sectionClass.classList.remove('active-link')
+    }
+  })
+}
+window.addEventListener('scroll', scrollActive)
+
+/*=============== DARK LIGHT THEME ===============*/
+const themeButton = document.getElementById('theme-button');
+const darkTheme = 'dark-theme';
+const iconTheme = 'ri-sun-line';
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme');
+const selectedIcon = localStorage.getItem('selected-icon');
+
+// We obtain the current theme that the interface has by vlidating the dark theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light';
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line';
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+  // If the validation is fulfilled, we ask what the issue was to know if we activated
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme);
+  themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme);
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+  // Add or remove the dark / icon theme
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme);
+  // We save the theme and the current icon that the user chose
+  localStorage.setItem('selected-theme', getCurrentTheme());
+  localStorage.setItem('selected-icon', getCurrentIcon());
+})
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '60px',
+  duration: 2500,
+  delay: 400
+  //reset: ture, // Animations repeat
+})
+
+sr.reveal(`.home__img, .newsletter__container, .footer__logo, .footer__description, .footer__content, .footer__info`);
+sr.reveal(`.home__data`, { origin: 'bottom' });
+sr.reveal(`.about__data, .recently__data`, { origin: 'left' });
+sr.reveal(`.about__img, .recently__img`, { origin: 'right' });
+sr.reveal(`.popular__card`, { interval: 100 });
